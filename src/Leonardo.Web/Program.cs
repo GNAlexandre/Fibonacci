aa8561e919e146c8aeaf95cb4f03653cb085d84c
Alexandre Tournel <alexandre.tournel@lacatholille.fr>
2024-12-17 16:28:11 +0100

    second commit

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -1,14 +1,18 @@
 using System.Text.Json.Serialization;
 using Leonardo;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FibonacciDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<Fibonacci>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 2!");
 
-app.MapGet("/fibonacci", async () =>
+app.MapGet("/fibonacci", async (Fibonacci fibonacci) =>
 {
-    var result = await new Fibonacci(new FibonacciDataContext()).RunAsync(new string[] { "42"});
+    var result = await fibonacci.RunAsync(new string[] { "42"});
     return Results.Json((List<FibonacciResult>?)result, FibonacciContext.Default.ListFibonacciResult);
 });
 
